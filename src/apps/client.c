186291324f7e2bc7e8e7a23d714f9fd38973e452
Janos Laube <janos.dev@gmail.com>
2015-01-28 11:46:28 +0100

    cleanup, fix error due to not setting fd to -1 on connect error
    Signed-off-by: Janos Laube <janos.dev@gmail.com>

--- src/apps/client.c
+++ src/apps/client.c
@@ -11,22 +11,62 @@
 
 #include "sdtl.h"
 
-#define	HOSTNAME	"127.0.0.1"
-#define PORT		1337
+#define	HOSTNAME	"localhost"
+#define PORT		"4242"
 
-int main_select(int argc, char* argv[], char* envp[])
-{
-	int cli_sock = 0;
+const char* address_families[] = {
+	"AF_UNSPEC",
+	"AF_UNIX",
+	"AF_INET",
+	"AF_AX25",
+	"AF_IPX",
+	"AF_APPLETALK",
+	"AF_NETROM",
+	"AF_BRIDGE",
+	"AF_ATMPVC",
+	"AF_X25",
+	"AF_INET6",
+	"AF_ROSE",
+	"AF_DECnet",
+	"AF_NETBEUI",
+	"AF_SECURITY",
+	"AF_KEY",
+	"AF_NETLINK",
+	"AF_PACKET",
+	"AF_ASH",
+	"AF_ECONET",
+	"AF_ATMSVC",
+	"AF_RDS",
+	"AF_SNA",
+	"AF_IRDA",
+	"AF_PPPOX",
+	"AF_WANPIPE",
+	"AF_LLC",
+	"AF_IB",
+	"(address family not defined)",
+	"AF_CAN",
+	"AF_TIPC",
+	"AF_BLUETOOTH",
+	"AF_IUCV",
+	"AF_RXRPC",
+	"AF_ISDN",
+	"AF_PHONET",
+	"AF_IEEE802154",
+	"AF_CAIF",
+	"AF_ALG",
+	"AF_NFC",
+	"AF_VSOCK"
+};
 
-	cli_sock = sio_connect4(HOSTNAME, PORT);
-	if (cli_sock < 0) {
-		pdie("Couldn't connect to server via IPv4.");
-	}
-
-	sio_close(cli_sock);
-	return 0;
-}
-
+const char* socket_types[] = {
+	"SOCK_STREAM",
+	"SOCK_DGRAM",
+	"SOCK_RAW",
+	"SOCK_RDM",
+	"SOCK_SEQPACKET",
+	"SOCK_DCCP",
+	"SOCK_PACKET"
+};
 
 int main(int argc, char* argv[], char* envp[])
 {
@@ -35,32 +75,41 @@ int main(int argc, char* argv[], char* envp[])
 
 	memset(&filter, 0, sizeof(filter));
 	filter.ai_family = AF_UNSPEC;
-	filter.ai_socktype = SOCK_STREAM;
+	filter.ai_socktype = SOCK_STREAM; /* TCP */
+	filter.ai_protocol = 0; /* any */
+	filter.ai_flags = 0; /* none */
 
-	if ((r = getaddrinfo("localhost", "4242", &filter, &servinfo)) != 0) {
+	if ((r = getaddrinfo(HOSTNAME, PORT, &filter, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
 		exit(1);
 	}
 
 	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) {
-			perror("socket");
-			continue;
+	#if 0
+		printf("ai_flags:\t%d\n", p->ai_flags);
+		printf("ai_family:\t%s\n", address_families[p->ai_family]);
+		printf("ai_socktype:\t%s\n", socket_types[p->ai_socktype]);
+		printf("ai_protocol:\t%d\n", p->ai_protocol);
+		printf("ai_canonname:\t%s\n", p->ai_canonname);
+	#endif
+		fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (fd < 0) {
+			perror("unable to create socket");
+			exit(~0);
 		}
-		if (connect(fd, p->ai_addr, p->ai_addrlen) == -1) {
+		if (connect(fd, p->ai_addr, p->ai_addrlen) < 0) {
 			close(fd);
+			fd = -1;
 			continue;
-		} else {
-			printf("connected\n");
+		} else
 			break;
-		}
 	}
+
 	if (fd < 0) {
-		perror("no socket\n");
+		perror("unable to connect to server");
 		freeaddrinfo(servinfo);
 		exit(~0);
 	}
-	printf("srv fd: %d\n", fd);
 
 	sdtl_write_fd_t sdtl_wfd;
 	int dbg_fd = fileno(stdout);
@@ -83,7 +132,13 @@ int main(int argc, char* argv[], char* envp[])
 
 	// loop over sdtl_write_chunk(&sdtl_wfd, unsigned char* data, uint16_t len)
 	for (i = 0; i < 10; ++i) {
-		//sdtl_write_chunk(&sdtl_wfd, (unsigned char*)"hello", 6);
+		/* TODO:
+		 *  	adjust SDTL to support resource streams without
+		 *	chunks in order to be able to make full use of the
+		 *	linux sendfile() API (it's possible with the current
+		 *	variable chunksize solution of SDTL octet streams but
+		 *	it would limit sendfile calls to 64kB chunks at max)
+		*/
 		sdtl_write_chunk(&sdtl_wfd, buf, 65535);
 	}
 
