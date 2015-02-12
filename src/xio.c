c6ba98c3166033dc43316e70caf25ad1604aa8d8
Janos Laube <janos.dev@gmail.com>
2015-02-12 17:34:24 +0100

    fix type
    Signed-off-by: Janos Laube <janos.dev@gmail.com>

--- src/xio.c
+++ src/xio.c
@@ -4,7 +4,7 @@
 int xwrite(int fd, const void* buf, size_t nbyte)
 {
 	size_t total = 0;
-	size_t written = 0;
+	ssize_t written = 0;
 	while (total != nbyte) {
 		written = write(fd, buf + total, nbyte - total);
 		if (written < 0) {
