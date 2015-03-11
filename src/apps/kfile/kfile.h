a8b182cd86b70ef8282162d4e1468442cd006993
Janos Laube <janos.dev@gmail.com>
2015-03-11 20:23:59 +0100

    .
    Signed-off-by: Janos Laube <janos.dev@gmail.com>

--- src/apps/kfile/kfile.h
+++ src/apps/kfile/kfile.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <string.h>
 
 #define KFILE_MAX_NAME_LENGTH	(256ull) /* 255 byte + 1 zero byte */
 #define KFILE_MAX_DIGEST_LENGTH	(128ull) /* 1024 bit */
@@ -133,6 +134,47 @@ static inline int check_uint8_size(uint16_t val)
 		return -1;
 
 	return 0;
+}
+
+static inline uint8_t get_pack_size(uint64_t n)
+{
+	/* is there a more efficient algorithm? */
+	if (n <= 0xff)
+		return 1;
+	if (n <= 0xffff)
+		return 2;
+	if (n <= 0xffffff)
+		return 3;
+	if (n <= 0xffffffff)
+		return 4;
+	if (n <= 0xffffffffff)
+		return 5;
+	if (n <= 0xffffffffffff)
+		return 6;
+	if (n <= 0xffffffffffffff)
+		return 7;
+	if (n <= 0xffffffffffffffff)
+		return 8;
+	return 0;
+}
+
+static inline size_t pack_uint64(uint64_t n, unsigned char* out)
+{
+	uint8_t pack_size = get_pack_size(n);
+
+	out[0] = pack_size - 1;
+	memcpy(out + 1, &n, pack_size);
+
+	return pack_size + 1;
+}
+
+static inline uint64_t unpack_uint64(unsigned char* in)
+{
+	uint64_t r = 0;
+	uint8_t pack_size = in[0] + 1;
+
+	memcpy(&r, in + 1, pack_size);
+	return r;
 }
 
 typedef int kfile_write_fd_t;
