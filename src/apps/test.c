9a422c5dd67480122734d4e806c729dd55da538c
Janos Laube <janos.dev@gmail.com>
2016-06-28 09:57:22 +0200

    .
    Signed-off-by: Janos Laube <janos.dev@gmail.com>

--- src/apps/test.c
+++ src/apps/test.c
@@ -1,69 +1,100 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/wait.h>
-
 #include "cstd.h"
-#include "socket/sio.h"
+#include "kfile.h"
 
-typedef struct slave_args_t {
-	int	listen_fd;
-} slave_args_t;
+#include <stdio.h>
+#include <string.h>
 
-int slaveproc(pid_t pid, pid_t pgid, void* data)
-{
-	slave_args_t* slave_args = (slave_args_t*) data;
-
-	if (!slave_args)
-		return -1;
-
-	info("slave [%d]: listen_fd %d", pid, slave_args->listen_fd);
-	for (;;) {
-		usleep(1000000);
-		//printf("[slave %u]: tic\n", pid);
-	}
-	return 0;
-}
+#include <socket/sio.h>
 
 int main(int argc, char* argv[], char* envp[])
 {
-	pid_t slave_pids[4];
-	int pipes[4];
-	int nslaves = 4;
-	int r = 0;
-	int listen_fd;
+#if 0
+	int fd0, fd1, fd2, fd3;
+
+	fd0 = file_create_rw_with_hidden_tmp("testfile0", 0, 00644);
+	write(fd0, "hello0\n", 7);
+
+	fd1 = file_create_rw_with_hidden_tmp("testfile1", 0, 00644);
+	write(fd1, "hello1\n", 7);
+
+	fd2 = file_create_rw_with_hidden_tmp("testfile2", 0, 00644);
+	write(fd2, "hello2\n", 7);
+
+	fd3 = file_create_rw_with_hidden_tmp("testfile3", 0, 00644);
+	write(fd3, "hello3\n", 7);
+
+
+	file_sync_and_close_all();
+#endif
+#if 0
+
+#if 1
+	unsigned char sdata[4096];
+	kfile_write_fd_t wfd;
+	kfile_create_opts_t kfcopts = {
+		.uuid			= 18446744073709551615ul,
+		.file_mode		= 0400,
+		.layout			= KFILE_LAYOUT_UUID_UINT64,
+		.version		= KFILE_VERSION_0_1,
+		.hash_function		= HASHSUM_SKEIN_512,
+		.digest_bytes		= 64,
+		.cipher_function	= BLK_CIPHER_AES,
+		.cipher_mode		= BLK_CIPHER_MODE_CTR,
+		.key_bytes		= 32,
+		.kdf_function		= KDF_SKEIN_1024,
+		.kdf_complexity		= 23,
+		.iobuf_size		= 65536,
+		.resource_name		= { "some_document.pdf" },
+		.low_entropy_pass	= { "test1234" }
+	};
+
+	memset(sdata, 'z', 4096);
+	sdata[4095] = 0;
+	wfd = kfile_create(&kfcopts);
+	if (wfd < 0)
+		pdie("kfile_create()");
+	kfile_update(wfd, sdata, sizeof(sdata));
+	kfile_write_digests_and_close(wfd);
+#endif
+
+#if 1
+	unsigned char rdata[4096];
+	kfile_read_fd_t rfd;
+	kfile_open_opts_t kfoopts = {
+		.layout			= KFILE_LAYOUT_UUID_UINT64,
+		.uuid			= 18446744073709551615ul,
+		.iobuf_size		= 65536,
+		.check_cipherdigest	= 1,
+		.low_entropy_pass	= { "test1234" }
+	};
+
+	rfd = kfile_open(&kfoopts);
+	if (rfd < 0)
+		pdie("kfile_open()");
+	kfile_read(rfd, rdata, 4096);
+	printf("resource: '%s'\n" , kfile_get_resource_name(rfd));
+	//printf("content: '%s'\n", rdata);
+	kfile_close(rfd);
+#endif
+//	printf("s: '%lu'\n", sizeof(kfile_kdf_header_t));
+#endif
+
+	int sock;
+
+	if (argc != 2)
+		return -1;
 
 	tcp_sock_opt_t sopt = {
-		.interface	= 0,	/* bind any */
+		.interface	= argv[1],
 		.port		= 1337,
-		.non_blocking	= 1,
+		.non_blocking	= 0,
 		.reuse_address	= 1,
 		.keep_alive	= 1,
 	};
 
-	listen_fd = sio_new_tcp_listening_socket(&sopt);
-	if (listen_fd < 0) {
-		pdie("sio_new_tcp_listening_socket()");
-	}
-
-	slave_args_t slave_args = {
-		.listen_fd = listen_fd,
-	};
-
-//	proc_fork_slaves(slave_pids, pipes, &nslaves, &slaveproc, &slave_args);
-
-#if 0
-	for (;;) {
-		usleep(1000000);
-	}
-#else
+	sock = sio_new_tcp_listening_socket(&sopt);
+	printf("sock: %d\n", sock);
 	getchar();
-#endif
-
-//	r = proc_terminate_slaves(slave_pids, nslaves);
-
-
-	printf("finished: %d.\n", r);
 
 	return 0;
 }
